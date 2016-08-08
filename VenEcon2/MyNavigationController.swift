6ed201be99a6b8551408590078755bec34e2b801
= <girish@girish-gupta.com>
2016-08-08 18:11:15 -0400

    Initial Commit

---
+++ VenEcon2/MyNavigationController.swift
@@ -0,0 +1,57 @@
+//
+//  MyNavigationController.swift
+//  SwiftSideMenu
+//
+//  Created by Evgeny Nazarov on 30.09.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: ENSideMenuNavigationController, ENSideMenuDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sideMenu = ENSideMenu(sourceView: self.view, menuViewController: MyMenuTableViewController(), menuPosition:.Left)
+        //sideMenu?.delegate = self //optional
+        sideMenu?.menuWidth = 180.0 // optional, default is 160
+        //sideMenu?.bouncingEnabled = false
+        //sideMenu?.allowPanGesture = false
+        // make navigation bar showing over side menu
+        view.bringSubviewToFront(navigationBar)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        print("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        print("sideMenuWillClose")
+    }
+    
+    func sideMenuDidClose() {
+        print("sideMenuDidClose")
+    }
+    
+    func sideMenuDidOpen() {
+        print("sideMenuDidOpen")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
